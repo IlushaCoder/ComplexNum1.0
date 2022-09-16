cc7d1a1b13eb8e8d5156f82d9c03f93b2beae3a0
Ilyas <awp.er@mail.ru>
2022-09-16 19:40:08 +0300

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        Complex a = new Complex(1, 1);
+        Complex b = new Complex(0, 1);
+        System.out.println(a.Arg());
+        System.out.println(b);
+
+
+    }
+}
